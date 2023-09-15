83cc0327b7bb7382ffc3d1bfc2e0372d9d996040
JayRPatelll <jaypatel9779ca@gmail.com>
2023-09-15 14:37:50 -0400

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by user232336 on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
